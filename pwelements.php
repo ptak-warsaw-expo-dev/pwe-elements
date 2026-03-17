04bca29daaeb80aeedb9f922ad638c874d46accc
Anton Melnychuk <87388822+antonmelnychuk1@users.noreply.github.com>
2026-03-17 12:52:38 +0100

    Add files via upload

--- pwelements.php
+++ pwelements.php
@@ -4,7 +4,7 @@
  * Plugin Name: PWE Elements
  * Plugin URI: https://github.com/ptak-warsaw-expo-dev/pwe-elements
  * Description: Adding a PWE elements to the website.
- * Version: 3.2.6
+ * Version: 3.2.7
  * Author: Marek Rumianek
  * Co-authors: Anton Melnychuk, Piotr Krupniewski, Jakub Choła
  * Author URI: github.com/RumianekMarek
